4d8a75ca8bb5891687b36f5af6ad1f93c215dcce
Mike <38372845+mbernier1@users.noreply.github.com>
2022-10-14 16:41:14 -0700

    Added player input

--- BrickGame/Assets/Scripts/Paddle.cs
+++ BrickGame/Assets/Scripts/Paddle.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    private const float PaddleSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,14 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float paddlePositionX = this.transform.position.x;
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+
+            paddlePositionX -= PaddleSpeed * Time.deltaTime;
+            this.transform.position = new Vector3(paddlePositionX,
+                    this.transform.position.y, this.transform.position.z);
+        }
         
     }
 }
